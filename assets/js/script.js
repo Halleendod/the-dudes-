dc4e890867da2cb8b9c9e5be98f9c022570f2f07
Shehan Rathnayake <shehandj.swd@gmail.com>
2023-07-31 11:32:25 +0530

    xtra small screens mobile responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,17 @@
 
 $(document).ready (()=> {
 
-    $("#hamb")
+    $(".hamburg").on("click", ()=>{
+        if($(".hamburg i").hasClass("fa-bars")) {
+            $(".menu-page").toggleClass("menu-active")
+            $(".hamburg i").addClass("fa-xmark").removeClass("fa-bars")
+            
+
+        } else if ($(".hamburg i").hasClass("fa-xmark")) {
+            $(".menu-page").toggleClass("menu-active")
+            $(".hamburg i").addClass("fa-bars").removeClass("fa-xmark")
+
+        }
+    })
 
 }) 
