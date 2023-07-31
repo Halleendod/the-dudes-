58582c18ae6c18738800b663ebcd9ba286c9ae5b
Shehan Rathnayake <shehandj.swd@gmail.com>
2023-07-31 22:09:57 +0530

    Privacy page updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
 $(document).ready (()=> {
 
+    // Mobile menu jquery function
+
     $(".hamburg").on("click", ()=>{
         if($(".hamburg i").hasClass("fa-bars")) {
             $(".menu-page").toggleClass("menu-active")
             $(".hamburg i").addClass("fa-xmark").removeClass("fa-bars")
-            
+        
 
         } else if ($(".hamburg i").hasClass("fa-xmark")) {
             $(".menu-page").toggleClass("menu-active")
